23860f56c93f7d3be5d7d1cde9d41c8628eaebdd
chauberahul123 <rahul.chaube2550@gmail.com>
2017-01-22 13:50:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,47 +1,63 @@
-(function() {
+ (function() {
   "use strict";
 
-  angular.module("LunchCheck", []).controller("LunchCheckController", LunchCheckController);
+  angular.module("ShoppingListCheckOff", [])
+    .controller("ToBuyShoppingController", ToBuyShoppingController)
+    .controller("AlreadyBoughtShoppingController", AlreadyBoughtShoppingController)
+    .service("ShoppingListCheckOffService", ShoppingListCheckOffService);
 
-  LunchCheckController.$inject = ["$scope"];
-  function LunchCheckController($scope) {
-    $scope.itemString = "";
-    $scope.nbOfItems = 0;
-    $scope.resultString;
-    // $scope.resultClass;
-    $scope.resultOk = null;
 
-    $scope.checkItems = function() {
-      //If anything entered in textbox
-      if($scope.itemString.length > 0) {
-        console.log("Items entered: " + $scope.itemString);
-        var itemArray = $scope.itemString.split(",");
-        console.log("Items as array: " + itemArray);
+  ToBuyShoppingController.$inject = ["ShoppingListCheckOffService"];
+  function ToBuyShoppingController(ShoppingListCheckOffService) {
+    var list = this;
 
-        $scope.resultString = checkItemCount(itemArray);
-        // var result = checkItemCount(itemArray);
-        // $scope.resultString = result.string;
-        // $scope.resultClass = result.class;
-        $scope.resultOk = true;
-      }
-      else
-      {
-        $scope.resultString = "Please enter data first";
-        // $scope.resultClass = "notOk";
-        $scope.resultOk = false;
-      }
+    list.items = ShoppingListCheckOffService.getItemsToBuy();
+
+    list.checkOff = function(itemIndex) {
+      console.log("Checking off item ", itemIndex);
+      ShoppingListCheckOffService.checkOff(itemIndex);
     };
   }
 
-  function checkItemCount(items) {
-    var itemCount = items.length;
-    console.log("Number of items entered: " + itemCount);
 
-    if(itemCount < 4)
-      // return { string: "Enjoy!", class: "ok" };
-      return "Enjoy!";
-    else
-      // return { string: "Too much!", class: "notOk" };
-      return "Too much!";
+  AlreadyBoughtShoppingController.$inject = ["ShoppingListCheckOffService"];
+  function AlreadyBoughtShoppingController(ShoppingListCheckOffService) {
+    var list = this;
+
+    list.items = ShoppingListCheckOffService.getItemsBought();
+  }
+
+
+  // ShoppingListCheckOffService.$inject = [];
+  function ShoppingListCheckOffService() {
+    var service = this;
+
+    var itemsToBuy = [ 
+      { name: "Haggis", quantity: 1 }, 
+      { name: "Neeps", quantity: 2 }, 
+      { name: "Tatties", quantity: 3 }, 
+      { name: "Drams", quantity: 4 }, 
+      { name: "Tablet", quantity: 5 }, 
+      { name: "Fudge", quantity: 6 }, 
+      { name: "Jelly Babies", quantity: 7 }, 
+      { name: "Salt & Vinegar Crisps", quantity: 8 }, 
+      { name: "Ale", quantity: 9 }, 
+      { name: "Plum Pudding", quantity: 10 } 
+    ];
+
+    var itemsBought = [];
+
+    service.getItemsToBuy = function () {
+      return itemsToBuy;
+    };
+
+    service.getItemsBought = function () {
+      return itemsBought;
+    };
+
+    service.checkOff = function (itemIndex) {
+      itemsBought.push(itemsToBuy[itemIndex]);
+      itemsToBuy.splice(itemIndex, 1);
+    };
   }
 })();
